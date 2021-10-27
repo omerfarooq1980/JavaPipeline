0401e0855d4e4b96cb346c254c8bd72b503c9271
omerfarooq1980 <omerfarooqm@gmail.com>
2021-10-27 21:56:08 +0530

    remove jenkinsfile

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 class HelloWorld{  
     public static void main(String args[]){  
      
-	 int a= args[0];
-	 int b= args[1];
+	 int a= Integer.parseInt(args[0]);
+	 int b= Integer.parseInt(args[1]);
 	 System.out.println("a="+a);  
 	 System.out.println("b="+b); 
 	 System.out.println("Adding a and b");  
