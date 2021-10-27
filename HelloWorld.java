444700ffb0b68740d49468e443a912312a6c6b80
omerfarooq1980 <omerfarooqm@gmail.com>
2021-10-27 21:53:58 +0530

    pipeline file added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 class HelloWorld{  
     public static void main(String args[]){  
      
-	 int a= arg[0];
-	 int b= arg[1];
+	 int a= args[0];
+	 int b= args[1];
 	 System.out.println("a="+a);  
 	 System.out.println("b="+b); 
 	 System.out.println("Adding a and b");  
